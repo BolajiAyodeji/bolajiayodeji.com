0997a5ab17aa72fcbf9d83a772b3ab1804ed24e9
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-05-16 19:06:30 +0100

    modified portfolio :fire:

--- public/portfolio/js/app.js
+++ public/portfolio/js/app.js
@@ -2,49 +2,41 @@
 
     "use strict";
 
-    // PRE LOADER
-    $(window).load(function () {
-        $('.preloader').fadeOut(1000); // set duration in brackets
-    });
-
-
     // WRAP INTRO TEXT
-    $('.ml1 .letters').each(function () {
+    $(".ml1 .letters").each(function () {
         $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
     });
 
     anime.timeline({
-            loop: true
-        })
+        loop: true
+    })
         .add({
-            targets: '.ml1 .letter',
+            targets: ".ml1 .letter",
             scale: [0.3, 1],
             opacity: [0, 1],
             translateZ: 0,
             easing: "easeOutExpo",
             duration: 600,
             delay: function (el, i) {
-                return 70 * (i + 1)
+                return 70 * (i + 1);
             }
         }).add({
-            targets: '.ml1 .line',
+            targets: ".ml1 .line",
             scaleX: [0, 1],
             opacity: [0.5, 1],
             easing: "easeOutExpo",
             duration: 700,
-            offset: '-=875',
+            offset: "-=875",
             delay: function (el, i, l) {
                 return 80 * (l - i);
             }
         }).add({
-            targets: '.ml1',
+            targets: ".ml1",
             opacity: 0,
             duration: 1000,
             easing: "easeOutExpo",
             delay: 1000
         });
-
-
 
     //INTRO ANIMATION
     let TxtType = function (el, toRotate, period) {
@@ -52,7 +44,7 @@
         this.el = el;
         this.loopNum = 0;
         this.period = parseInt(period, 10) || 3000;
-        this.txt = '';
+        this.txt = "";
         this.tick();
         this.isDeleting = false;
     };
@@ -67,7 +59,7 @@
             this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
 
-        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+        this.el.innerHTML = "<span class=\"wrap\">" + this.txt + "</span>";
 
         let that = this;
         let delta = 200 - Math.random() * 100;
@@ -79,7 +71,7 @@
         if (!this.isDeleting && this.txt === fullTxt) {
             delta = this.period;
             this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
+        } else if (this.isDeleting && this.txt === "") {
             this.isDeleting = false;
             this.loopNum++;
             delta = 500;
@@ -91,10 +83,10 @@
     };
 
     window.onload = function () {
-        let elements = document.getElementsByClassName('typewrite');
+        let elements = document.getElementsByClassName("typewrite");
         for (let i = 0; i < elements.length; i++) {
-            let toRotate = elements[i].getAttribute('data-type');
-            let period = elements[i].getAttribute('data-period');
+            let toRotate = elements[i].getAttribute("data-type");
+            let period = elements[i].getAttribute("data-period");
             if (toRotate) {
                 new TxtType(elements[i], JSON.parse(toRotate), period);
             }
@@ -108,8 +100,8 @@
     //END INTRO ANIMATION
 
     // MENU
-    $('.navbar-collapse a').on('click', function () {
-        $(".navbar-collapse").collapse('hide');
+    $(".navbar-collapse a").on("click", function () {
+        $(".navbar-collapse").collapse("hide");
     });
 
     $(window).scroll(function () {
@@ -125,12 +117,11 @@
         horizontalScrolling: false,
     });
 
-
     // MAGNIFIC POPUP
-    $('.image-popup').magnificPopup({
-        type: 'image',
+    $(".image-popup").magnificPopup({
+        type: "image",
         removalDelay: 300,
-        mainClass: 'mfp-with-zoom',
+        mainClass: "mfp-with-zoom",
         gallery: {
             enabled: true
         },
@@ -138,7 +129,7 @@
             enabled: true, // By default it's false, so don't forget to enable it
 
             duration: 300, // duration of the effect, in milliseconds
-            easing: 'ease-in-out', // CSS transition easing function
+            easing: "ease-in-out", // CSS transition easing function
 
             // The "opener" function should return the element from which popup will be zoomed in
             // and to which popup will be scaled down
@@ -146,7 +137,7 @@
             opener: function (openerElement) {
                 // openerElement is the element on which popup was initialized, in this case its <a> tag
                 // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                return openerElement.is('img') ? openerElement : openerElement.find('img');
+                return openerElement.is("img") ? openerElement : openerElement.find("img");
             }
         }
     });
@@ -154,24 +145,14 @@
 
     // SMOOTH SCROLL
     $(function () {
-        $('.custom-navbar a, .footer-link li a, .scroll-to-top, #home a').on('click', function (event) {
+        $(".custom-navbar a, .footer-link li a, .scroll-to-top, #home a").on("click", function (event) {
             let $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 49
+            $("html, body").stop().animate({
+                scrollTop: $($anchor.attr("href")).offset().top - 49
             }, 1000);
             event.preventDefault();
         });
     });
 
-    // Scroll to top button appear
-    $(document).scroll(function () {
-        let scrollDistance = $(this).scrollTop();
-        if (scrollDistance > 100) {
-            $('.scroll-to-top').fadeIn();
-        } else {
-            $('.scroll-to-top').fadeOut();
-        }
-    });
-    
 })(jQuery);
 
