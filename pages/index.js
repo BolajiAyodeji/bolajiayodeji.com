34e2e527dec25a8dc2cc8d87eb030fa82862d036
Bolaji Ayodeji <sirbeejay1@gmail.com>
2020-12-23 09:10:42 +0100

    feat: setup firebase :fire:

--- pages/index.js
+++ pages/index.js
@@ -54,13 +54,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           © 2018 - Today | Copyright Bolaji Ayodeji.
-        </a>
       </footer>
     </div>
   );
