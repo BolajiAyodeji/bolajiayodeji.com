95f7e70e35c72f5fbb823791f4a2d3f4c1e54fe6
Bolaji Ayodeji <sirbeejay1@gmail.com>
2020-12-24 06:31:52 +0100

    style(home): add some styles and fix description on mobile

--- pages/index.js
+++ pages/index.js
@@ -21,33 +21,33 @@ export default function Home() {
           <a href="https://commercelayer.io" target="_blank" rel="noopener noreferrer">
             Commerce Layer
           </a>
-          . I'm passionate about sharing knowledge, documentation, web
-          engineering, JAMstack, communities, and FOSS. I create technical
-          content on my <a href="https://blog.bolajiayodeji.com" target="_blank" rel="noopener noreferrer">Blog</a> &&{" "}
+          . He's passionate about sharing knowledge, documentation, web
+          engineering, JAMstack, communities, and FOSS. He creates technical
+          content on his <a href="https://blog.bolajiayodeji.com" target="_blank" rel="noopener noreferrer">Blog</a> &&{" "}
           <a href="https://www.youtube.com/c/bolajiayodeji" target="_blank" rel="noopener noreferrer">YouTube</a>, speak
           at some conferences/meetups and build technical communities.
         </p>
 
         <div className={styles.grid}>
-          <a href="http://blog.bolajiayodeji.com" className={styles.card}>
-            <h3>Blog &rarr;</h3>
+          <a href="https://blog.bolajiayodeji.com" className={styles.card}>
+            <h3>✍🏾 Blog</h3>
             <p>
               Read web engineering, JAMstack, productivity and random articles.
             </p>
           </a>
 
           <a href="https://github.com/BolajiAyodeji" className={styles.card}>
-            <h3>GitHub &rarr;</h3>
+            <h3>👨🏾‍💻 GitHub</h3>
             <p>Checkout or contribute to my open source projects.</p>
           </a>
 
           <a href="https://twitter.com/iambolajiayo" className={styles.card}>
-            <h3>Twitter &rarr;</h3>
+            <h3>🐦 Twitter</h3>
             <p>Follow me, interact with my tweets and maybe send a DM.</p>
           </a>
 
           <a href="https://patreon.com/bolajiayodeji" className={styles.card}>
-            <h3>Patreon &rarr;</h3>
+            <h3>💰 Patreon</h3>
             <p>Become my patron and $upport my work.</p>
           </a>
         </div>
