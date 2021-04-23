70200e93c042a0531787a4a94e2aebf5007627e8
Bolaji Ayodeji <sirbeejay1@gmail.com>
2021-04-23 11:00:08 +0100

    Upgrade firebase SDK :sparkles:

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script src="/__/firebase/8.2.1/firebase-app.js"></script>
-          <script src="/__/firebase/8.2.1/firebase-analytics.js"></script>
+          <script src="/__/firebase/8.4.1/firebase-app.js"></script>
+          <script src="/__/firebase/8.4.1/firebase-analytics.js"></script>
           <script src="/__/firebase/init.js"></script>
           <script src="noflash.js" />
         </body>
