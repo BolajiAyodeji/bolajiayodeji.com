fefd26dcad708e704634d2def594ca74f5fb0c09
Bolaji Ayodeji <sirbeejay1@gmail.com>
2021-04-23 17:37:36 +0100

    refactor: clean up and deploy to Netlify

--- pages/_document.js
+++ pages/_document.js
@@ -5,13 +5,27 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          <script src="noflash.js" />
+          <body>
           <Main />
           <NextScript />
-          <script src="/__/firebase/8.4.1/firebase-app.js"></script>
-          <script src="/__/firebase/8.4.1/firebase-analytics.js"></script>
-          <script src="/__/firebase/init.js"></script>
-          <script src="noflash.js" />
         </body>
       </Html>
     );
